463e7e0e78eaac2384abd2b0fdd64c4ca3c8b006
sukmatulak129 <sukmatulak@gmail.com>
2026-04-21 18:08:31 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,9 +7,11 @@ import streamlit as st
 st.set_page_config(page_title="E-Commerce Analysis Dashboard", layout="wide")
 
 # --- LOAD DATA ---
+# --- LOAD DATA ---
 @st.cache_data
 def load_data():
-    df = pd.read_csv("main_data.csv")
+    url = "https://raw.githubusercontent.com/sukmatulak129/Analisis_Data_E-Commerce/main/dashboard/main_data.csv"
+    df = pd.read_csv(url)
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     df['month'] = df['order_purchase_timestamp'].dt.month
     df['year'] = df['order_purchase_timestamp'].dt.year
